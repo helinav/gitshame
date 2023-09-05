fb0ac0c40ccc502e40f17e6b7322c2ce41da7258
helinav <helina.valb@gmail.com>
2023-09-05 13:33:37 +0300

    Gitshame masterharu projekti alustamine

---
+++ src/main/java/com/example/gitshame/GitshameBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitshame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitshameBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitshameBackApplication.class, args);
+	}
+
+}
